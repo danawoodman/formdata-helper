40ef3a6ef520d618d3f666da17f0b5d681119afd
Dana Woodman <dana@danawoodman.com>
2022-09-01 14:53:55 -0700

    Add custom truthy/falsy matchers and lots more tests
    Better type support

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,11 @@
-import { parseFormData, type StructuredFormData } from "./index";
+import { Configuration, parseFormData, type StructuredFormData } from "./index";
 import { describe, expect, test } from "vitest";
 
-function createFormData(data: {}): FormData {
+function createFormData(data?: {}): FormData {
 	const formData = new FormData();
+
+	if (!data || !Object.keys(data).length) return formData;
+
 	for (const [key, val] of Object.entries(data)) {
 		if (Array.isArray(val)) {
 			for (const v of val) {
@@ -15,61 +18,113 @@ function createFormData(data: {}): FormData {
 	return formData;
 }
 
-const c = createFormData;
+const f = createFormData;
 
 describe("parseFormData", () => {
 	const tests: {
 		name: string;
 		formdata?: any;
-		defaultData?: StructuredFormData;
+		config?: Configuration<any>;
 		expected: any;
 	}[] = [
 		// Ensure sane fallback behavior for passing in weird values.
-		{ name: "undefined", formdata: undefined, expected: {} },
-		{ name: "null", formdata: null, expected: {} },
-		{ name: "empty string", formdata: "", expected: {} },
-		{ name: "number", formdata: 32.5, expected: {} },
+		{ name: "handles undefined", formdata: undefined, expected: {} },
+		{ name: "handles null", formdata: null, expected: {} },
+		{ name: "handles empty string", formdata: "", expected: {} },
+		{ name: "handles number", formdata: 32.5, expected: {} },
 
 		// Ensure that we can parse a simple form.
-		{ name: "empty form data", formdata: new FormData(), expected: {} },
+		{ name: "handles empty form data", formdata: f(), expected: {} },
 		{
 			name: "simple text only form",
-			formdata: c({ name: "Jane Doe" }),
+			formdata: f({ name: "Jane Doe" }),
 			expected: { name: "Jane Doe" },
 		},
 		{
 			name: "coerce numbers",
-			formdata: c({ age: 35, height: 6.1 }),
+			formdata: f({ age: 35, height: 6.1 }),
 			expected: { age: 35, height: 6.1 },
 		},
 		{
 			name: "coerce booleans",
-			formdata: c({ admin: true, active: false }),
+			formdata: f({ admin: true, active: false }),
 			expected: { admin: true, active: false },
 		},
 		{
-			name: "coerce grouped fields",
-			formdata: c({ favoriteFruits: ["Banana", "Mango"] }),
+			name: "coerces grouped fields",
+			formdata: f({ favoriteFruits: ["Banana", "Mango"] }),
 			expected: { favoriteFruits: ["Banana", "Mango"] },
 		},
 		{
-			name: "coerce grouped fields (numeric)",
-			formdata: c({ radioStations: [147.9, 102.1] }),
+			name: "coerces grouped fields (numeric)",
+			formdata: f({ radioStations: [147.9, 102.1] }),
 			expected: { radioStations: [147.9, 102.1] },
 		},
 
-		// Check passing in default object
+		// Check passing in default
 		{
 			name: "empty form data with default data",
-			formdata: new FormData(),
-			defaultData: { a: true },
+			formdata: f(),
+			config: { defaults: { a: true } },
 			expected: { a: true },
 		},
+		{
+			name: "form overrides defaults",
+			formdata: f({ a: false }),
+			config: { defaults: { a: true, b: 12 } },
+			expected: { a: false, b: 12 },
+		},
+
+		// Custom truthy matchers
+		{
+			name: "custom truthy/false matchers",
+			formdata: f({ a: "yes", b: "0", c: "y", d: "false" }),
+			config: {
+				truthy: ["yes", "y", "1"],
+				falsy: ["no", "n", "0"],
+			},
+			expected: { a: true, b: false, c: true, d: "false" },
+		},
+
+		// File support
 	];
 
 	for (const t of tests) {
 		test(t.name, () => {
-			expect(parseFormData(t.formdata, t.defaultData)).toEqual(t.expected);
+			expect(parseFormData(t.formdata, t.config)).toEqual(t.expected);
 		});
 	}
+
+	describe("type interface", () => {
+		test("happy path", () => {
+			const data = { name: "Jane Doe", age: 36 };
+			type T = { name: string; age: number };
+			const resp = parseFormData<T>(f(data));
+			expect(resp).toEqual(data);
+		});
+
+		test("works with defaults", () => {
+			const defaults = { age: 36 };
+			type T = { name: string; age: number };
+			const resp = parseFormData<T>(f(), { defaults });
+			expect(resp.name).toBeUndefined();
+			expect(resp.age).toBe(36);
+		});
+
+		// TODO: how to test TS errors?
+		// test("generates TS error with mismatched types", () => {
+		// 	type T = { username: string; age: number };
+		// 	const resp = parseFormData<T>(f({ admin: false}));
+		// 	expect(resp.admin).toBeUndefined();
+		// 	expect(resp.username).toBeUndefined();
+		// });
+
+		// test("generates TS error with mismatched types", () => {
+		// 	const defaults = { admin: true };
+		// 	type T = { username: string };
+		// 	const resp = parseFormData<T>(f(), { defaults });
+		// 	expect(resp.username).toBeUndefined();
+		// 	expect(resp.admin).toBeUndefined();
+		// });
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,120 @@ export type StructuredFormValue =
 
 export type StructuredFormData = Record<string, StructuredFormValue>;
 
-export function parseFormData(
+export interface Configuration<T> {
+	/**
+	 * Can be a partial object of `T` type.
+	 *
+	 * This is useful if you want to always guarantee that certain fields are
+	 * present in the output.
+	 */
+	defaults?: Partial<T>;
+
+	/**
+	 * What values to use when determining a "falsy" value to coerce to `false`.
+	 *
+	 * Examples:
+	 * - `["false", "no", "n", "0"]`
+	 *
+	 * Default is `"false"`
+	 */
+	falsy?: string | string[];
+
+	/**
+	 * What values to use when determining a "truthy" value to coerce to `true`.
+	 *
+	 * Examples:
+	 * - `["true", "yes", "y", "1"]`
+	 *
+	 * Default is `"true"`
+	 */
+	truthy?: string | string[];
+}
+
+export function parseFormData<T extends StructuredFormData>(
 	data?: FormData,
-	defaultData?: StructuredFormData
-) {
+	configuration?: Configuration<T>
+): Partial<T> {
 	// TODO: throw or return empty object/undefined/null?
 	if (!data || !(data instanceof FormData)) return {};
 
-	const initial = { ...defaultData } as StructuredFormData;
+	const config = {
+		truthy: "true",
+		falsy: "false",
+		defaults: {},
+		...configuration,
+	} as Required<Configuration<T>>;
+	const truthy = toArray(config.truthy);
+	const falsy = toArray(config.falsy);
+	const defaults = config.defaults;
 
-	return [...data.entries()].reduce((data, [k, v]) => {
-		let value: StructuredFormValue = v;
-		if (v === "true") value = true;
-		if (v === "false") value = false;
-		if (!isNaN(Number(v))) value = Number(v);
+	return [...data.entries()].reduce(
+		(data, [k, v]) => {
+			/**
+			 * Coerce the values to their primitive types.
+			 *
+			 * Start with booleans before we
+			 */
+			let value: StructuredFormValue = v;
 
-		// For grouped fields like multi-selects and checkboxes, we need to
-		// store the values in an array.
-		if (k in data) {
-			const val = data[k];
-			value = Array.isArray(val) ? [...val, value] : [val, value];
-		}
+			const isString = typeof v === "string";
 
-		data[k] = value;
+			if (isString && truthy.includes(v)) value = true;
+			else if (isString && falsy.includes(v)) value = false;
+			// if (v === "true") value = true;
+			// if (v === "false") value = false;
+			else if (!isNaN(Number(v))) value = Number(v);
 
-		return data;
-	}, initial);
+			if (k in data) {
+				/**
+				 * If the key exists in the defaults object, we check to see if
+				 * it is not an array value because we don't want to accidentally
+				 * create an array of values when the expectation is it will return
+				 * a single value.
+				 *
+				 * For example, this call:
+				 *  const data = new FormData();
+				 * 	data.append("a", "foo");
+				 * 	parseFormData(data, { defaults: { a: "bar" } })
+				 *
+				 * Should result in this output:
+				 * 	{ a: "foo" }
+				 *
+				 * Not:
+				 * 	{ a: ["foo", "bar"] }
+				 */
+				if (k in defaults && !Array.isArray(defaults[k])) {
+					data[k] = value;
+				} else {
+					/**
+					 * For grouped fields like multi-selects and checkboxes, we need to
+					 * store the values in an array.
+					 *
+					 * For example, this call:
+					 *  const data = new FormData();
+					 * 	data.append("a", "foo");
+					 * 	data.append("a", "bar");
+					 * 	parseFormData(data)
+					 *
+					 * Should result in this output:
+					 * 	{ a: ["foo", "bar"] }
+					 *
+					 * Not:
+					 * 	{ a: "foo" } or { a: "bar" }
+					 */
+					const val = data[k];
+					value = Array.isArray(val) ? [...val, value] : [val, value];
+				}
+			}
+
+			data[k] = value;
+
+			return data;
+		},
+		{ ...defaults }
+	);
+}
+
+function toArray(val: string | string[]): string[] {
+	return Array.isArray(val) ? val : [val];
 }
